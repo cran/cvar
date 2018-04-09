56f0044132edd59b9acf3db0febfd302e5d17dd1
Georgi N. Boshnakov <georgi.boshnakov@manchester.ac.uk>
2018-04-09 15:01:48 +0000

    version 0.1-1

---
+++ NEWS.md
@@ -0,0 +1,8 @@
+# Changes in Version 0.1-1 (CRAN)
+
+- added a doi to DESCRIPTION.
+
+
+# Changes in Version 0.1-0
+
+- first public release.
---
+++ inst/auto/REFERENCES.el
@@ -0,0 +1,10 @@
+(TeX-add-style-hook
+ "REFERENCES"
+ (lambda ()
+   (LaTeX-add-bibitems
+    "acerbi2002expected"
+    "PerformanceAnalytics2018"
+    "VaRES2013"
+    "actuarJSS2008"))
+ :bibtex)
+
